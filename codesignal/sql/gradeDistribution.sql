b2a379db3fe91e1bd22cb9fd19cd4a331fdd5a7e
codecaiine <akabrouyannickn@gmail.com>
2022-07-04 22:00:44 +0000

    Resolve gradeDistribution challenge

---
+++ codesignal/sql/gradeDistribution.sql
@@ -0,0 +1,5 @@
+CREATE PROCEDURE solution()
+BEGIN
+SELECT Name, ID FROM Grades WHERE Final > (Midterm1 + Midterm2)/2 AND Final > 
+((Midterm1+Midterm2)/2 +Final)/2 ORDER BY SUBSTRING(Name, 1, 3), ID ASC;
+END
