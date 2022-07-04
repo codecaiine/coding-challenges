f45d94bee22f000fdc3afe93da6008d8be85c186
codecaiine <akabrouyannickn@gmail.com>
2022-07-04 22:07:07 +0000

    Resolve mischievousNephews challenge

---
+++ codesignal/sql/mischievousNephews.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE solution()
+BEGIN
+SELECT WEEKDAY(mischief_date) AS weekday,
+        mischief_date,
+        author,
+        title FROM mischief ORDER BY 
+        weekday,
+        FIELD(author, 'Huey', 'Dewey' ,'Louie'), mischief_date,title;
+END
